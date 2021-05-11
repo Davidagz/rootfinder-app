c01c1a279da685e8b94946680dd574c51d7c2e8b
David Garza <david.garza@huskers.unl.edu>
2021-05-11 00:08:28 -0500

    Removed unused method in backend

--- backend/app.py
+++ backend/app.py
@@ -63,17 +63,3 @@ def conditions(solution_type):
     else:
         return False
 
-
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
